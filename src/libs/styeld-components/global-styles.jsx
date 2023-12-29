0282ffe305e045716d21b619305f44bbebd8c8cd
yunshin <2younsin@gmail.com>
2023-12-30 04:50:49 +0900

    feat: GlobalStyles & DesignToken 작성

---
+++ src/libs/styeld-components/global-styles.jsx
@@ -0,0 +1,55 @@
+import { createGlobalStyle } from 'styled-components'
+import '../../assets/fonts/font.css'
+import { BREAK_POINT, FONT_SIZE, FONT_WEIGHT } from './tokens'
+
+const GlobalStyles = createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+
+        font-family: 'KkomaNabi';
+        font-size: ${FONT_SIZE.md};
+        font-weight: ${FONT_WEIGHT.normal};
+    }
+    html {
+        font-size: 80%;
+        @media screen and (max-width: ${BREAK_POINT.md}){
+          font-size: 65%;
+        }
+        @media screen and (max-width: ${BREAK_POINT.sm}){
+          font-size: 50%;
+        }
+    }
+    ul, li {
+        list-style: none;
+    }
+
+    h1 {
+      font-weight: ${FONT_WEIGHT.bold};
+      font-size: ${FONT_SIZE.xl};
+    }
+    h2 {
+      font-weight: ${FONT_WEIGHT.bold};
+      font-size: ${FONT_SIZE.lg};
+    }
+    h3 {
+      font-weight: ${FONT_WEIGHT.bold};
+      font-size: ${FONT_SIZE.bg};
+    }
+    h4 {
+      font-weight: ${FONT_WEIGHT.bold};
+      font-size: ${FONT_SIZE.md};
+    }
+    button {
+        border: none;
+    }
+    input {
+        outline: none;
+    }
+    textarea {}
+    p, div, span {
+      font-size: ${FONT_SIZE.md};
+    }
+`
+export default GlobalStyles
---
+++ src/libs/styeld-components/tokens.js
@@ -0,0 +1,27 @@
+const COLOR = {
+	main: '#FFFBF6',
+	success: '#3BF137',
+	error: '#F14D37'
+}
+
+const BREAK_POINT = {
+	sm: '480px', // small
+	md: '896px', // medium
+	lg: '1152px' // large
+}
+
+const FONT_SIZE = {
+	sm: '1.3rem', // small
+	md: '1.8rem', // medium
+	bg: '2.4rem', // big
+	lg: '3.1rem', // large
+	xl: '4.8rem' // extreme large
+}
+
+const FONT_WEIGHT = {
+	thin: 100,
+	normal: 500,
+	bold: 1000
+}
+
+export { BREAK_POINT, COLOR, FONT_SIZE, FONT_WEIGHT }
