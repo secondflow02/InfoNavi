a613cde3770ea77c508d9e4467cedbaf456c0172
yunshin <2younsin@gmail.com>
2023-12-31 04:19:11 +0900

    depend: recoli 의존성 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { RouterProvider } from 'react-router-dom'
+import { RecoilRoot } from 'recoil'
 import router from './libs/react-router-dom/router'
 import GlobalStyles from './libs/styeld-components/global-styles'
 
 const App = () => {
 	return (
-		<>
+		<RecoilRoot>
 			<GlobalStyles />
 			<RouterProvider router={router} />
-		</>
+		</RecoilRoot>
 	)
 }
 
