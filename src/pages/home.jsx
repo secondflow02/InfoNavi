65d9b80d6e2cacd0612773a9e005ed25003efe9f
Team <50646145+TransparentDeveloper@users.noreply.github.com>
2024-01-02 04:14:02 +0900

    fix: 올바른 이름으로 다시 생성

---
+++ src/pages/home.jsx
@@ -0,0 +1,46 @@
+import styled from 'styled-components'
+import Center from '../components/center'
+import Compressor from '../components/compressor'
+import SearchBox from '../components/search-box'
+import Spacer from '../components/spacer'
+import { COLOR } from '../libs/styeld-components/tokens'
+import LogoImg from '/logo/logo-766x152.webp'
+
+const Home = () => {
+	return (
+		<Compressor>
+			<Center
+				$bgColor={COLOR.grayScale[1500] + 'BF'}
+				$radius='3rem'
+				$variant='outline'
+			>
+				<S.FlexColumnContainer>
+					<S.Logo src={LogoImg} width='30%' />
+					<Spacer $height='3rem' />
+					<SearchBox $width='50%' $radius='3rem' />
+				</S.FlexColumnContainer>
+			</Center>
+		</Compressor>
+	)
+}
+
+export default Home
+
+const Logo = styled.img`
+	width: 30%;
+`
+
+const FlexColumnContainer = styled.div`
+	position: relative;
+	display: flex;
+
+	flex-direction: column;
+	align-items: center;
+
+	gap: 1rem;
+`
+
+const S = {
+	Logo,
+	FlexColumnContainer
+}
