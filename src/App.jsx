6f897e448207e9f52b3b9674df5c6f37c59b1de7
yunshin <2younsin@gmail.com>
2024-01-03 05:05:14 +0900

    fix: RecoilRoot 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import { RouterProvider } from 'react-router-dom'
-import { RecoilRoot } from 'recoil'
 import router from './libs/react-router-dom/router'
 import GlobalStyles from './libs/styeld-components/global-styles'
 
 const App = () => {
 	return (
-		<RecoilRoot>
+		<>
 			<GlobalStyles />
 			<RouterProvider router={router} />
-		</RecoilRoot>
+		</>
 	)
 }
 
