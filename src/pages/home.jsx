cf6bd06a66206ae25e12620204e6f0592947560c
yunshin <2younsin@gmail.com>
2024-01-03 07:29:10 +0900

    design: 스크린 크기에 따른 반응형 코드 작성

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -3,17 +3,13 @@ import Center from '../components/center'
 import Compressor from '../components/compressor'
 import SearchManager from '../components/search-manager'
 import Spacer from '../components/spacer'
-import { COLOR } from '../libs/styeld-components/tokens'
+import { BREAK_POINT, COLOR } from '../libs/styeld-components/tokens'
 import LogoImg from '/logo/logo-766x152.webp'
 
 const Home = () => {
 	return (
 		<Compressor>
-			<Center
-				$bgColor={COLOR.grayScale[1500] + 'BF'}
-				$radius='3rem'
-				$variant='outline'
-			>
+			<Center>
 				<S.FlexColumnContainer>
 					<S.Logo src={LogoImg} width='30%' />
 					<Spacer $height='3rem' />
@@ -27,15 +23,29 @@ const Home = () => {
 export default Home
 
 const Logo = styled.img`
-	width: 30%;
+	width: 40%;
 `
 
 const FlexColumnContainer = styled.div`
-	position: relative;
-	display: flex;
+	width: 100%;
+	height: 100%;
+	@media screen and (max-width: ${BREAK_POINT.md}) {
+		height: 80%;
+	}
+	@media screen and (max-width: ${BREAK_POINT.sm}) {
+		height: 50%;
+	}
 
+	position: relative;
+
+	display: flex;
 	flex-direction: column;
 	align-items: center;
+	justify-content: center;
+
+	background-color: ${COLOR.grayScale[1500] + 'BF'};
+	border: 1px solid ${COLOR.grayScale[1200]};
+	border-radius: 3rem;
 
 	gap: 1rem;
 `
